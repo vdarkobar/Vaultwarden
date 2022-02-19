f30b1de462b7ac742401eb02fa838ff8a64c20c0
Darko Vukovic <vdarkobar@gmail.com>
2022-02-19 21:59:21 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+RED='\033[0;31m'
+echo -ne "${RED}Enter Domain name: "; read DNAME; \
+echo -ne "${RED}Enter Vaultwarden Subdomain: "; read SDNAME; \
+echo -ne "${RED}Enter Vaultwarden Port Number (VWPORTN:80): "; read VWPORTN; \
+sed -i "s|01|${DNAME}|" .env && \
+sed -i "s|02|${SDNAME}|" .env && \
+sed -i "s|03|${VWPORTN}|" .env && \
+rm README.md && \
+TOKEN=$(openssl rand -base64 48); sed -i "s|CHANGE_ADMIN_TOKEN|${TOKEN}|" .env
+while true; do
+    read -p "Execute 'docker-compose up -d' now? (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo docker-compose up -d; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
